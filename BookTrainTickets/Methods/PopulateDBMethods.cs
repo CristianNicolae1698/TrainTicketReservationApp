18364ccb660967262fa468140a75d267d6d0f390
CristianNicolae <cristiannicolae1698@gmail.com>
2022-09-05 16:10:30 +0300

    New Commit STG

---
+++ BookTrainTickets/Methods/PopulateDBMethods.cs
@@ -0,0 +1,87 @@
+﻿using BookTrainTickets.DataAccess;
+using BookTrainTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTrainTickets.Methods
+{
+    public class PopulateDBMethods
+    {
+        private static List<Train> CreateTrains()
+        {
+            var trains = new List<Train>();
+            var t1 = new Train
+            {
+
+                TrainNumber = "727",
+                TrainType = "InterRegio"
+            };
+            var t2 = new Train
+            {
+
+                TrainNumber = "828",
+                TrainType = "InterCity"
+            };
+            var t3 = new Train
+            {
+
+                TrainNumber = "929",
+                TrainType = "InterEuropean"
+            };
+
+            trains.Add(t1);
+            trains.Add(t2);
+            trains.Add(t3);
+            return trains;
+
+        }
+
+        private static List<Station> CreateStations()
+        {
+            var stations = new List<Station>();
+            var st1 = new Station
+            {
+                StationName = "Bucharest"
+            };
+            var st2 = new Station
+            {
+                StationName = "Craiova"
+            };
+
+            stations.Add(st1);
+            stations.Add(st2);
+
+            return stations;
+        }
+
+
+        public static void CreateRoute()
+        {
+
+
+
+            var route1 = new Route
+            {
+                RouteName = "Craiova - Bucharest",
+
+
+
+            };
+
+
+            route1.Trains.AddRange(CreateTrains());
+            route1.Stations.AddRange(CreateStations());
+            using (var db = new BookingContext())
+            {
+                db.Routes.Add(route1);
+
+                db.SaveChanges();
+            }
+
+
+        }
+    }
+}
---
+++ BookTrainTickets/Models/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTrainTickets.Models
+{
+    public class Booking
+    {
+        public Guid Id{ get; set; }
+        public List<Client> Clients{ get; set; }=new List<Client>();
+
+        public List<Route> Routes { get; set; }=new List<Route>();
+
+        public List<Train> Trains { get; set; }=new List<Train>();
+
+        public string BookingDate { get; set; }
+
+        public int Price { get; set; }
+        public bool CheckedIn { get; set; }
+
+    }
+}
