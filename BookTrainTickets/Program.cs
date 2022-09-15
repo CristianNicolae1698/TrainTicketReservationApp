953dc335efdb441df8841c2cd0d6161ad56fa40e
CristianNicolae <cristiannicolae1698@gmail.com>
2022-09-15 19:03:29 +0300

    Adding web api project type to the solution

--- BookTrainTickets/Program.cs
+++ BookTrainTickets/Program.cs
@@ -2,6 +2,9 @@
 using BookTrainTickets.Models;
 using BookTrainTickets.Methods;
 using System;
+using BookTrainTickets.Core.IConfiguration;
+using FluentAssertions.Common;
+
 namespace BookTrainTickets
 {
     public class Program
@@ -9,33 +12,35 @@ namespace BookTrainTickets
         public static void Main(string[] args)
 
         {
+
+
+
+            //PopulatingSampleDb.AddTrainToDb("PanEuropean", "919");
+            //PopulatingSampleDb.AddTrainToDb("Softronic", "232");
+            //PopulatingSampleDb.AddTrainToDb("NavigationExp", "656");
+
+            //PopulatingSampleDb.AddStationToDb("Craiova");
+            //PopulatingSampleDb.AddStationToDb("Bucuresti");
+            //PopulatingSampleDb.AddRouteToDb("Craiova", "Bucuresti", "656");
+
+
+            //PopulatingSampleDb.AddStationToDb("Constanta");
+            //PopulatingSampleDb.AddStationToDb("Brasov");
+            //PopulatingSampleDb.AddRouteToDb("Constanta", "Brasov", "232");
+
+            //PopulatingSampleDb.AddStationToDb("Constanta");
+            //PopulatingSampleDb.AddRouteToDb("Bucuresti", "Constanta", "919");
+
+
+            //Console.WriteLine("Done processing");
+            //Console.ReadLine();
+
             
-            //PopulatingSampleDb.AddTrainToDb("EuroMaster", "525");
-            //PopulatingSampleDb.AddTrainToDb("MagLev", "212");
-            //PopulatingSampleDb.AddTrainToDb("HighRail", "515");
-            //PopulatingSampleDb.AddTrainToDb("CommonRail", "616");
-            //PopulatingSampleDb.AddTrainToDb("Rapide", "717");
-            //PopulatingSampleDb.AddTrainToDb("InterCity", "818");
-
-            PopulatingSampleDb.AddTrainToDb("PanEuropean", "919");
-            PopulatingSampleDb.AddTrainToDb("Softronic", "232");
-            PopulatingSampleDb.AddTrainToDb("NavigationExp", "656");
-
-            PopulatingSampleDb.AddStationToDb("Craiova");
-            PopulatingSampleDb.AddStationToDb("Bucuresti");
-            PopulatingSampleDb.AddRouteToDb("Craiova", "Bucuresti", "656");
 
 
-            PopulatingSampleDb.AddStationToDb("Constanta");
-            PopulatingSampleDb.AddStationToDb("Brasov");
-            PopulatingSampleDb.AddRouteToDb("Constanta", "Brasov", "232");
-
-            PopulatingSampleDb.AddStationToDb("Constanta");
-            PopulatingSampleDb.AddRouteToDb("Bucuresti", "Constanta", "919");
 
 
-            Console.WriteLine("Done processing");
-            Console.ReadLine();
+
         }
 
 
---
+++ TrainTicketsAppWebAPI/Controllers/RoutesController.cs
@@ -0,0 +1,47 @@
+﻿using BookTrainTickets.Core.IConfiguration;
+using Microsoft.AspNetCore.Mvc;
+using BookTrainTickets.Models;
+
+
+namespace TrainTicketsAppWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+
+    public class RoutesController : ControllerBase
+    {
+        private readonly ILogger<RoutesController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RoutesController(ILogger<RoutesController> logger, IUnitOfWork unitOfWork)
+        {
+
+            _logger = logger;
+            _unitOfWork=unitOfWork;
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateRoute (BookTrainTickets.Models.Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                route.Id = Guid.NewGuid();
+                await _unitOfWork.Routes.Add(route);
+                await _unitOfWork.CompleteAsync();
+
+                return CreatedAtAction("GetItem", new {route.Id}, route);
+
+            }
+
+            return new JsonResult("Something went wrong") { StatusCode = 500};
+
+        }
+
+
+
+    }
+}
